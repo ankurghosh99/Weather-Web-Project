a38af3cf894a4681efb843597ca93d41a6113313
Ankur Ghosh <ankurghosh.airtel.mng@gmail.com>
2025-11-11 21:03:24 +0530

    Change initial weatherData to use placeholders
    Updated initial state of weatherData with placeholders.

--- WeatherApp.jsx
+++ WeatherApp.jsx
@@ -4,14 +4,14 @@ import WeatherInfo from './Weatherinfo';
 
 export default function WeatherApp(){
     const[weatherData, setWeatherData]=useState({
-            City:"Kolkata",
-            Temp:18.97,
-            TempMin:18.97,
-            TempMax:18.97,
-            Humidity:77,
-            Pressure:1013,
-            FeelsLike:18.93,
-            Weather:"mist",
+            City:"_____",
+            Temp:"___",
+            TempMin:"___",
+            TempMax:"___",
+            Humidity:"___",
+            Pressure:"___",
+            FeelsLike:"___",
+            Weather:"___",
     });
 
     function updateWeatherData(newData){
